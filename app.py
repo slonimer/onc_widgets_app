45ceb5eeedd16e16c329eb388913cccc2a409eaf
slonimer <slonimer@uvic.ca>
2025-10-20 15:04:24 -0700

    Added input elements

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import streamlit as st
 
 st.title("Oceans 3.0 Open API Playground")
 
-
-st.header(":blue[Discovery Services]")
-
-st.subheader(":green[Return locations]")
-
 st.markdown(":blue-badge[GET] `/locations`")
+
+location_code = st.text_input("locationCode", placeholder="FGPD")
+
+if st.button("Run", key="location_button"):
+    st.write(f"you input {location_code}")
+    #Location code does not show up here if just FGPD, because is just a place holder
 
 st.divider()
 
@@ -17,3 +18,10 @@ st.header(":blue[Real-time Services]")
 st.subheader(":green[Return archivefiles]")
 
 st.markdown(":blue-badge[GET] `/archivefile`")
+
+device_code = st.text_input("deviceCode", value="BPR_BC")
+last_days = st.number_input("last days", value=4)
+
+if st.button("Run", key="archivefile_button"):
+    st.write(f"you input {device_code}")
+    st.write(f"you input {last_days}")
